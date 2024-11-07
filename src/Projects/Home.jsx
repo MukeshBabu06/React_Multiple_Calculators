a958bd39a10247cb6b4e453c0772b31445f0b858
MukeshBabu06 <mukesh.shaankar@gmail.com>
2024-11-07 16:46:39 +0530

    Update Home.jsx

--- src/Projects/Home.jsx
+++ src/Projects/Home.jsx
@@ -8,11 +8,11 @@ export default function Myhome (){
         <main>
             <article>
                 <h1>Calculators</h1>
-                <div><Link className="linkstyle" to="/PrjSimpleint">Simple Interest Calculator</Link></div>
-                <div><Link className="linkstyle" to="/PrjCompint">Compound Interest Calculator</Link></div>
+                <div><Link className="linkstyle" to="/PrjSimpleint">Simple Interest <br/> Calculator</Link></div>
+                <div><Link className="linkstyle" to="/PrjCompint">Compound Interest <br/> Calculator</Link></div>
                 <div><Link className="linkstyle" to="/PrjHomeLoan">Home loan EMI Calculator</Link></div>
                 <div><Link className="linkstyle" to="/PrjEMICal">EMI Calculator</Link></div>
-                <div><Link className="linkstyle" to="/PrjMiscal">Post Office Mis Calculator</Link></div>
+                <div><Link className="linkstyle" to="/PrjMiscal">Post Office <br/> Mis Calculator</Link></div>
                 <div><Link className="linkstyle" to="/PrjPPFcal">PPF Calculator</Link></div>
                 <div><Link className="linkstyle" to="/PrjSipcal">SIP Calculator</Link></div>
             </article>
